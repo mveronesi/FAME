af5b87f69c4b200a2f9d8c5dfe311124a00b246d
ryma-bmzz <ryma.boumazouza@airbus.com>
2025-12-17 17:23:02 +0100

    feat(notebooks): add CIFAR10 CNN experiment and ResNet-2B model

---
+++ notebooks/configs/definitions.py
@@ -0,0 +1,20 @@
+# geometric transformations
+ROTATE = 'rotate'
+TRANSLATE_X = 'translateX'
+TRANSLATE_Y = 'translateY'
+SCALE = 'scale'
+SHEAR = 'shear'
+CONTRAST = 'contrast'
+BRIGHTNESS = 'brightness'
+
+# datasets
+MNIST = 'mnist'
+CIFAR10 = 'cifar10'
+TINY = 'tiny-imagenet'
+DRIVING = 'driving'
+
+# training methods
+AUGMENTED = 'augmented'
+PGD_A = 'pgda'
+IBP_A = 'ibpa'
+CGT_IBP = 'cgtibp'
