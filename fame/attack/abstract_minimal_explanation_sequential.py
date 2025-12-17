Fix(normalization): Update functions to accept data mean and std
Adapts multiple attack and domain computation functions to receive the data's mean and standard deviation, enabling correct inversion/scaling for normalized input data.

fame/attack/abstract_minimal_explanation_sequential.py
@@ -22,6 +22,8 @@ def find_closest_xai(
     data_format: int = "channels_first",
     n_class: int = 10,
     traversal_order: str = "greedy",
+    means = None, 
+    stddev = None
 ) -> tuple[list[int], list[int]]:
     """Identifies a minimal set of robust features required to prevent adversarial attacks.
 
@@ -70,8 +72,12 @@ def find_closest_xai(
 
     n_in_wo_channel: int = int(input_sample.shape[-1] / channel)
 
-    lower_bound: np.ndarray = np.maximum(input_sample - eps, 0 * input_sample)
-    upper_bound: np.ndarray = np.minimum(input_sample + eps, 0 * input_sample + 1)
+    if means is None and stddev is None:
+        lower_bound: np.ndarray = np.maximum(input_sample - eps, 0 * input_sample)
+        upper_bound: np.ndarray = np.minimum(input_sample + eps, 0 * input_sample + 1)
+    else:
+        lower_bound = np.maximum(input_sample - eps, - (means/stddev))
+        upper_bound = np.minimum(input_sample + eps, ((1-means)/stddev))
 
     # start by attacking everything
     adv_pred_everything: np.array = attack(
@@ -82,6 +88,7 @@ def find_closest_xai(
         gt_label=gt_label,
         eps=eps,
         method=method,
+        device=device
     )  # (1,)
     if adv_pred_everything[0] == gt_label:
         print("no attacks could be find, skip the search")

fame/attack/utils.py
@@ -106,6 +106,8 @@ def get_attacks_bounds(
     remaining_indices: list[int],
     channel: int = 1,
     data_format: str = "channels_first",
+    means = None, 
+    stddev = None
 ) -> tuple[np.ndarray, np.ndarray, np.ndarray]:
     """Constructs a batch of input domains for testing the vulnerability of single features.
 
@@ -142,9 +144,13 @@ def get_attacks_bounds(
     n_in_wo_channel: int = int(input_sample.shape[-1] / channel)
     batch_size = len(remaining_indices)
 
-    lower_bound_input_ = np.maximum(input_sample - eps, 0 * input_sample)
-    upper_bound_input_ = np.minimum(input_sample + eps, 0 * input_sample + 1)
-
+    if means is None and stddev is None:
+        lower_bound_input_ = np.maximum(input_sample - eps, 0 * input_sample)
+        upper_bound_input_ = np.minimum(input_sample + eps, 0 * input_sample + 1)
+    else:
+        lower_bound_input_: np.ndarray = np.maximum(np.copy(input_sample) - eps, - (means/stddev))
+        upper_bound_input_: np.ndarray = np.minimum(np.copy(input_sample) + eps, ((1-means)/stddev))
+        
     # reshape according to data_format
     lower_bound_c: np.array
     upper_bound_c: np.array

fame/batch_free/free.py
@@ -375,6 +375,8 @@ def free_iteratively_k_features(
     method: str = "greedy",
     refining_domain: bool = True,
     verbose: int = 0,
+    means=None, 
+    stddev=None
 ) -> tuple[list[int], list[int]]:
     """Iteratively finds the largest possible set of robust features for a given input.
 
@@ -417,8 +419,13 @@ def free_iteratively_k_features(
     """
     n_in_with_channel: int = input_sample.shape[-1]
     n_in_wo_channel: int = int(n_in_with_channel / channel)
-    lower_bound_input: np.ndarray = np.maximum(np.copy(input_sample) - eps, 0 * input_sample)
-    upper_bound_input: np.ndarray = np.minimum(np.copy(input_sample) + eps, 0 * input_sample + 1)
+
+    if means is None and stddev is None:
+        lower_bound_input: np.ndarray = np.maximum(np.copy(input_sample) - eps, 0 * input_sample)
+        upper_bound_input: np.ndarray = np.minimum(np.copy(input_sample) + eps, 0 * input_sample + 1)
+    else:
+        lower_bound_input: np.ndarray = np.maximum(np.copy(input_sample) - eps, - (means/stddev))
+        upper_bound_input: np.ndarray = np.minimum(np.copy(input_sample) + eps, ((1-means)/stddev))
 
     cardinality: np.ndarray = np.array([i for i in range(1, n_in_wo_channel - len(free_indices))])
     abstract_set = np.zeros((1,))  # temporary
@@ -462,8 +469,12 @@ def free_iteratively_k_features(
                 )
 
             # overwrite the bounds in case they have been corrupted
-            lower_bound_input = np.maximum(input_sample - eps, 0 * input_sample)
-            upper_bound_input = np.minimum(input_sample + eps, 0 * input_sample + 1)
+            if means is None and stddev is None:
+                lower_bound_input = np.maximum(input_sample - eps, 0 * input_sample)
+                upper_bound_input = np.minimum(input_sample + eps, 0 * input_sample + 1)
+            else:
+                lower_bound_input: np.ndarray = np.maximum(np.copy(input_sample) - eps, - (means/stddev))
+                upper_bound_input: np.ndarray = np.minimum(np.copy(input_sample) + eps, ((1-means)/stddev))
 
             abstract_set = free_at_once_k_features(
                 model=model,
@@ -485,8 +496,13 @@ def free_iteratively_k_features(
     # while we find one singleton (we add the one with the least impact according to abstract bound)
     # finish with singleton search
     decomon_singleton = get_abstract_model_singleton(model=model, n_class=n_class)
-    lower_bound_input = np.maximum(input_sample - eps, 0 * input_sample)
-    upper_bound_input = np.minimum(input_sample + eps, 0 * input_sample + 1)
+    if means is None and stddev is None:
+        lower_bound_input = np.maximum(input_sample - eps, 0 * input_sample)
+        upper_bound_input = np.minimum(input_sample + eps, 0 * input_sample + 1)
+    else:
+        lower_bound_input: np.ndarray = np.maximum(np.copy(input_sample) - eps, - (means/stddev))
+        upper_bound_input: np.ndarray = np.minimum(np.copy(input_sample) + eps, ((1-means)/stddev))
+        
     singleton_free_index: list
     singleton_free_index = free_with_binary_search(
         model=model,